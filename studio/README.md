edd08d76135bc00980a119bf5349dcbc3cf8611c
Sanity.io <hello@sanity.io>
2020-10-22 14:26:23 -0400

    Initial commit

---
+++ studio/README.md
@@ -0,0 +1,1 @@
+influify-sanity-webapp-studio
---
+++ studio/dashboardConfig.js
@@ -0,0 +1,47 @@
+export default {
+  widgets: [
+    {name: 'structure-menu'},
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '5f91cec5ca83f613fcfb30c3',
+                  title: 'Sanity Studio',
+                  name: 'influify-sanity-webapp-studio',
+                  apiId: '1e92c26d-75a5-4d3c-83bd-bcb833574b5f'
+                },
+                {
+                  buildHookId: '5f91cec5f3632814a6e6842b',
+                  title: 'Blog Website',
+                  name: 'influify-sanity-webapp',
+                  apiId: '0fca08a6-62b0-4dea-adc5-6626cce3e1ed'
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/Influify/influify-sanity-webapp',
+            category: 'Code'
+          },
+          {title: 'Frontend', value: 'https://influify-sanity-webapp.netlify.app', category: 'apps'}
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recent blog posts', order: '_createdAt desc', types: ['post']},
+      layout: {width: 'medium'}
+    }
+  ]
+}
